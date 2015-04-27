c730291c4a3b251aef03eb44bfc9cbe156fe2172
Juan Calderon <jjcalderon29@gmail.com>
2015-04-26 20:29:45 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## y is a square matrix in the global environment
+## flag is used to verify if inverse  has been change
 makeCacheMatrix <- function(x = matrix()) {
-
+  flag <- "FALSE"
+  x <<- y
+  print(x)
 }
 
 
 ## Write a short comment describing this function
+## uses matrix x, and calculate inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if (flag == "FALSE)" {
+    flag <- "TRUE"
+    x <<- y
+    print(X)
+    print (y)
+    inverse <<- solve(x)
+    } print(inverse)
 }
